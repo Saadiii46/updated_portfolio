7663047f9740ae474b70688f71401b1088baaba6
Muhammad Saad Khalid <szsaadiii0@gmail.com>
2025-07-05 23:59:01 +0500

    Initial Commit

---
+++ src/constants/index.js
@@ -0,0 +1,318 @@
+export const myProjects = [
+  {
+    id: 1,
+    title: "Full Stack Zoom Clone",
+    description:
+      "A full stack Zoom clone that enables real-time video conferencing and chat functionality",
+    subDescription: [
+      "Developed a real-time video conferencing platform using WebRTC, enabling peer-to-peer video and audio calls with high performance.",
+      "Integrated interactive chat functionality powered by Stream, allowing users to send and receive messages during meetings seamlessly.",
+      "Designed a responsive frontend with Tailwind CSS, enhancing user experience.",
+    ],
+    href: "",
+    logo: "",
+    image: "/assets/projects/zoom-clone.png",
+    tags: [
+      {
+        id: 1,
+        name: "NextJs#",
+        path: "/assets/logos/next.svg",
+      },
+      {
+        id: 2,
+        name: "Typescript",
+        path: "/assets/logos/typescript.svg",
+      },
+      {
+        id: 3,
+        name: "Stream.io",
+        path: "/assets/logos/stream.png",
+      },
+      {
+        id: 4,
+        name: "TailwindCSS",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Authentication & Authorization System",
+    description:
+      "A secure authentication and authorization system using Auth0 for seamless user management.",
+    subDescription: [
+      "Integrated Auth0 for authentication, supporting OAuth, JWT, and multi-factor authentication.",
+      "Implemented role-based access control (RBAC) for fine-grained user permissions.",
+      "Developed a React-based frontend with Tailwind CSS for a sleek user experience.",
+      "Connected to a secure SQLite database for user data storage.",
+    ],
+    href: "",
+    logo: "",
+    image: "/assets/projects/auth-system.jpg",
+    tags: [
+      {
+        id: 1,
+        name: "Auth0",
+        path: "/assets/logos/auth0.svg",
+      },
+      {
+        id: 2,
+        name: "React",
+        path: "/assets/logos/react.svg",
+      },
+      {
+        id: 3,
+        name: "SQLite",
+        path: "/assets/logos/sqlite.svg",
+      },
+      {
+        id: 4,
+        name: "TailwindCSS",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Movie App",
+    description: "A modern, interactive web application built with React 19",
+    subDescription: [
+      "Developed a fully interactive Single Page Application using React19.",
+      "Implemented API interactions for a robust backend.",
+      "Designed responsive UI components with Tailwind CSS for an enhanced UX.",
+    ],
+    href: "",
+    logo: "",
+    image: "/assets/projects/project-2.png",
+    tags: [
+      {
+        id: 1,
+        name: "React",
+        path: "/assets/logos/react.svg",
+      },
+      {
+        id: 2,
+        name: "Javascript",
+        path: "/assets/logos/javascript.svg",
+      },
+
+      {
+        id: 3,
+        name: "Movie database",
+        path: "/assets/logos/sql.svg",
+      },
+      {
+        id: 4,
+        name: "TailwindCSS",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "Multi Login System",
+    description: "A lightweight Multi Login System designed for Schools.",
+    subDescription: [
+      "Developed a multi-tenant full stack system that creates a unique database for each school.",
+      "Developed dynamic role-based authentication using JWT, allowing distinct login access",
+      "Developed custom dashboards where users can add, view, and manage staff and student data",
+      "Developed the entire platform using the MERN stack (MongoDB, Express, React, Node.js) with responsive UI",
+    ],
+    href: "",
+    logo: "",
+    image: "/assets/projects/game-engine.jpg",
+    tags: [
+      {
+        id: 1,
+        name: "React",
+        path: "/assets/logos/react.svg",
+      },
+      {
+        id: 2,
+        name: "Javascript",
+        path: "/assets/logos/javascript.svg",
+      },
+      {
+        id: 3,
+        name: "MongoDB",
+        path: "/assets/logos/mongo.svg",
+      },
+      {
+        id: 4,
+        name: "Tailwind",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+    ],
+  },
+  // {
+  //   id: 5,
+  //   title: "WordPress Custom Theme",
+  //   description:
+  //     "A fully customizable WordPress theme optimized for performance and SEO.",
+  //   subDescription: [
+  //     "Developed a responsive WordPress theme using HTML5, CSS3, and JavaScript.",
+  //     "Integrated Tailwind CSS for modern styling and UI enhancements.",
+  //     "Optimized SEO and page speed using Vite.js for fast builds.",
+  //     "Implemented custom widgets and plugin compatibility for extended functionality.",
+  //   ],
+  //   href: "",
+  //   logo: "",
+  //   image: "/assets/projects/wordpress-theme.jpg",
+  //   tags: [
+  //     {
+  //       id: 1,
+  //       name: "WordPress",
+  //       path: "/assets/logos/wordpress.svg",
+  //     },
+  //     {
+  //       id: 2,
+  //       name: "HTML5",
+  //       path: "/assets/logos/html5.svg",
+  //     },
+  //     {
+  //       id: 3,
+  //       name: "CSS3",
+  //       path: "/assets/logos/css3.svg",
+  //     },
+  //     {
+  //       id: 4,
+  //       name: "Vite.js",
+  //       path: "/assets/logos/vitejs.svg",
+  //     },
+  //   ],
+  // },
+  // {
+  //   id: 6,
+  //   title: "Online Learning Platform",
+  //   description:
+  //     "A web application that allows users to enroll in courses, watch video lectures, and take quizzes.",
+  //   subDescription: [
+  //     "Built using Blazor WebAssembly for a seamless SPA experience.",
+  //     "Implemented video streaming with Azure Media Services.",
+  //     "Added a quiz system with dynamic question generation and real-time grading.",
+  //     "Integrated Stripe API for secure payment processing.",
+  //   ],
+  //   href: "",
+  //   logo: "",
+  //   image: "/assets/projects/elearning.jpg",
+  //   tags: [
+  //     {
+  //       id: 1,
+  //       name: "Blazor",
+  //       path: "/assets/logos/blazor.svg",
+  //     },
+  //     {
+  //       id: 2,
+  //       name: "Azure",
+  //       path: "/assets/logos/azure.svg",
+  //     },
+  //     {
+  //       id: 3,
+  //       name: "Stripe",
+  //       path: "/assets/logos/stripe.svg",
+  //     },
+  //     {
+  //       id: 4,
+  //       name: "TailwindCSS",
+  //       path: "/assets/logos/tailwindcss.svg",
+  //     },
+  //   ],
+  // },
+];
+
+export const mySocials = [
+  {
+    name: "WhatsApp",
+    href: "",
+    icon: "/assets/socials/whatsApp.svg",
+  },
+  {
+    name: "Linkedin",
+    href: "https://www.linkedin.com/in/muhammad-saad-khalid-8a6288282/",
+    icon: "/assets/socials/linkedIn.svg",
+  },
+  // {
+  //   name: "Instagram",
+  //   href: "https://www.instagram.com/ali.sanatidev/reels/",
+  //   icon: "/assets/socials/instagram.svg",
+  // },
+];
+
+export const experiences = [
+  {
+    title: "Full Stack Developer",
+    job: "Full Stack Applications",
+    date: "2023-204",
+    contents: [
+      "Designed and developed scalable web applications using the MERN stack (MongoDB, Express.js, React.js, Node.js), ensuring responsive UI and efficient backend logic.",
+      "Built and consumed RESTful APIs and integrated third-party services, enabling smooth communication between frontend and backend components.",
+      " Implemented authentication, authorization, and role-based access control using JWT and middleware, enhancing app security and user management.",
+      "Collaborated in Agile environments, using Git, GitHub, and Jira to manage version control, task tracking, and efficient team communication.",
+    ],
+  },
+  {
+    title: "Frontend Developer",
+    job: "Frontend Designs",
+    date: "2024-2025",
+    contents: [
+      "Developed responsive and interactive user interfaces using React.js and Tailwind CSS, ensuring optimal performance across all devices.",
+      " Transformed UI/UX designs into pixel-perfect web pages, collaborating closely with designers and backend developers for seamless integration.",
+      "Implemented state management using tools like Redux or Context API, maintaining smooth and predictable app behavior.",
+      "Optimized web performance and accessibility, using best practices like lazy loading, semantic HTML, and Lighthouse audits.",
+      "Delivered remote features like over-the-air updates, real-time tracking, and remote start capabilities.",
+    ],
+  },
+  {
+    title: "Freelance Developer",
+    job: "Self-Employed",
+    date: "2025-Present",
+    contents: [
+      "Created a personal portfolio using Three.js, React, Vite, and WebAPI to showcase technical expertise.",
+      "Continuously enhancing technical skills and expanding expertise in modern web development and back-end technologies.",
+    ],
+  },
+];
+export const reviews = [
+  {
+    name: "Maryam",
+    username: "@maryam",
+    body: "I've never seen anything like this before. It's amazing. I love it.",
+    img: "https://robohash.org/maryam",
+  },
+  {
+    name: "Amna",
+    username: "@amna",
+    body: "I don't know what to say. I'm speechless. This is amazing. Great person",
+    img: "https://robohash.org/amna",
+  },
+  {
+    name: "Rayn",
+    username: "@rayn",
+    body: "Great Experience.",
+    img: "https://robohash.org/rayn",
+  },
+  {
+    name: "Duha",
+    username: "@duha",
+    body: "This is hands down the best thing I've experienced. Highly recommend!",
+    img: "https://robohash.org/duha",
+  },
+  {
+    name: "Sajil",
+    username: "@sajil",
+    body: "Incredible work! The attention to detail is phenomenal.",
+    img: "https://robohash.org/sajil",
+  },
+  {
+    name: "Saboor",
+    username: "@saboor",
+    body: "This exceeded all my expectations. Absolutely stunning!",
+    img: "https://robohash.org/saboor",
+  },
+  {
+    name: "Janie",
+    username: "@janie",
+    body: "Simply breathtaking. The best decision I've made in a while.",
+    img: "https://robohash.org/janie",
+  },
+];
